3300881fd8ec5a0793e7f21cb982b0a7efbf93fb
Cindy <ctarud@uc.cl>
2019-06-12 21:47:26 -0400

    Movie detail ready

---
+++ swinfo/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('movies/<int:movie_id>/', views.detail, name='detail'),
+    path('characters/<character_id>/', views.character_detail, name='character_detail'),
+    path('ships/<starship_id>/', views.ship_detail, name='ship_detail'),
+    path('planets/<planet_id>/', views.planet_detail, name='planet_detail'),
+    path('search/', views.search),
+]
---
+++ swinfo/views.py
@@ -0,0 +1,283 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import requests
+
+#
+# def index(request):
+#     return HttpResponse("Hello World! Welcome to the poll index :D")
+url = 'https://swapi-graphql-integracion-t3.herokuapp.com/'
+def index(request):
+    query = {"query":
+                """
+                {
+                  allFilms {
+                    edges {
+                      node {
+                        episodeID
+                        title
+                        director
+                        producers
+                        releaseDate
+                      }
+                    }
+                  }
+                }
+                """
+                }
+
+    response = requests.post(url = url, json = query)
+
+    movies = response.json()
+
+    return render(request, 'swinfo/home.html', {
+        'movies': movies["data"]["allFilms"]["edges"],
+    })
+
+def detail(request, movie_id):
+    query = {"query":
+                """
+                {
+                  allFilms {
+                    edges {
+                      node {
+                        episodeID
+                        openingCrawl
+                        title
+                        director
+                        producers
+                        releaseDate
+                        episodeID
+                        title
+                        director
+                        producers
+                        releaseDate
+                        planetConnection {
+                            planets {
+                                name
+                                id
+                            }
+                        }
+                        starshipConnection {
+                            starships {
+                                name
+                                id
+                            }
+                        }
+                        characterConnection {
+                            characters {
+                                name
+                                id
+                            }
+                        }
+
+
+
+                      }
+                    }
+                  }
+                }
+                """
+                }
+    response = requests.post(url = url, json = query)
+    movies = response.json()["data"]["allFilms"]["edges"]
+    characters = {}
+    starships = {}
+    planets = {}
+    for movie in movies:
+        if movie["node"]['episodeID'] == movie_id:
+            correct_movie = movie
+    for character in correct_movie['node']['characterConnection']['characters']:
+        characters.update({character["name"]: character["id"]})
+
+    for starship in correct_movie['node']['starshipConnection']['starships']:
+        starships.update({starship['name']:starship['id']})
+
+    for planet in correct_movie['node']['planetConnection']['planets']:
+        planets.update({planet['name']:planet['id']})
+
+    return render(request, 'swinfo/detail.html', {
+        'movie': correct_movie["node"],
+        'characters':characters,
+        'starships':starships,
+        'planets':planets,
+    })
+
+def character_detail(request, character_id):
+
+    query = {"query":
+            """
+            {
+              allPeople {
+                edges
+                {
+                  node {
+                   name
+                    id
+                    height
+                    mass
+                    eyeColor
+                    hairColor
+                    skinColor
+                    birthYear
+                    gender
+                    homeworld {
+                        name
+                    }
+                  }
+
+                }
+              }
+             }
+             """
+            }
+
+    response = requests.post(url = url, json = query)
+    characters = response.json()["data"]["allPeople"]["edges"]
+    for character in characters:
+        if character["node"]["id"] == character_id:
+            true_character = character["node"]
+    # character = response.json()
+    # planet = character['homeworld']
+    # homeworld = requests.get(planet).json()
+    # films = []
+    # starships = {}
+    #
+    # for film in character['films']:
+    #     movie = requests.get(film)
+    #     films.append(movie.json())
+    #
+    # for starship in character['starships']:
+    #     ships = requests.get(starship)
+    #     head, partition, tail = starship.partition("starships/")
+    #     starships_id = tail[:-1]
+    #     starships.update({ships.json()['name']:starships_id})
+
+    return render(request, 'swinfo/character_detail.html',{
+        'character':true_character,
+        # 'homeworld':homeworld,
+        # 'films':films,
+        # 'starships':starships,
+    })
+
+def ship_detail(request, starship_id):
+    response = requests.get('https://swapi.co/api/starships/'+str(starship_id))
+    ship = response.json()
+    films = []
+    pilots = {}
+
+    for film in ship['films']:
+        movie = requests.get(film)
+        films.append(movie.json())
+
+    for pilot in ship['pilots']:
+        piloto = requests.get(pilot)
+        head, partition, tail = pilot.partition("people/")
+        piloto_id = tail[:-1]
+        pilots.update({piloto.json()['name']:piloto_id})
+
+
+    return render(request, 'swinfo/ship_detail.html',{
+        'ship':ship,
+        'films':films,
+        'pilots':pilots,
+    })
+
+def planet_detail(request, planet_id):
+    response = requests.get('https://swapi.co/api/planets/'+str(planet_id))
+    planet = response.json()
+    films = []
+    residents = {}
+
+    for film in planet['films']:
+        movie = requests.get(film)
+        films.append(movie.json())
+
+    for resident in planet['residents']:
+        residente = requests.get(resident)
+        head, partition, tail = resident.partition("people/")
+        residente_id = tail[:-1]
+        residents.update({residente.json()['name']:residente_id})
+
+    return render(request, 'swinfo/planet_detail.html',{
+        'planet':planet,
+        'films':films,
+        'residents':residents,
+    })
+
+
+def search(request):
+    if 'q' in request.GET:
+        q = request.GET['q']
+        movies_result = requests.get('https://swapi.co/api/films/'+'?search='+q)
+        movies = movies_result.json()
+        movies_sources = []
+
+        for pelicula in movies["results"]:
+            movies_sources.append(pelicula)
+
+        while movies["next"]:
+            movies = requests.get(movies["next"]).json()
+            for pelicula in movies["results"]:
+                movies_sources.append(pelicula)
+
+        character_result = requests.get('https://swapi.co/api/people/'+'?search='+q)
+        characters = character_result.json()
+        characters_sources = []
+        for personaje in characters["results"]:
+            head, partition, tail = personaje["url"].partition("people/")
+            character_id = tail[:-1]
+            personaje.update({"id":character_id})
+            characters_sources.append(personaje)
+        while characters["next"]:
+            characters = requests.get(characters["next"]).json()
+            for personaje in characters["results"]:
+                head, partition, tail = personaje["url"].partition("people/")
+                character_id = tail[:-1]
+                personaje.update({"id":character_id})
+                characters_sources.append(personaje)
+
+
+        planet_result = requests.get('https://swapi.co/api/planets/'+'?search='+q)
+        planets = planet_result.json()
+        planets_sources = []
+        for planeta in planets["results"]:
+            head, partition, tail = planeta["url"].partition("planets/")
+            planet_id = tail[:-1]
+            planeta.update({"id":planet_id})
+            planets_sources.append(planeta)
+        while planets["next"]:
+            planets = requests.get(planets["next"]).json()
+            for planeta in planets["results"]:
+                head, partition, tail = planeta["url"].partition("planets/")
+                planet_id = tail[:-1]
+                planeta.update({"id":planet_id})
+                planets_sources.append(planeta)
+
+
+        ship_result = requests.get('https://swapi.co/api/starships/'+'?search='+q)
+        ships = ship_result.json()
+        ships_sources = []
+        for nave in ships["results"]:
+            head, partition, tail = nave["url"].partition("starships/")
+            ship_id = tail[:-1]
+            nave.update({"id":ship_id})
+            ships_sources.append(nave)
+        while ships["next"]:
+            ships = requests.get(ships["next"]).json()
+            for nave in ships["results"]:
+                head, partition, tail = nave["url"].partition("starships/")
+                ship_id = tail[:-1]
+                nave.update({"id":ship_id})
+                ships_sources.append(nave)
+
+
+        return render(request, 'swinfo/search.html', {
+            'movies' : movies_sources,
+            'characters' : characters_sources,
+            'planets' : planets_sources,
+            'ships' : ships_sources,
+        })
+
+
+    else:
+        return HttpResponse("Por favor ingresa un término para buscar.")
